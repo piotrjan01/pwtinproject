c9729deb0494fd5ecb15ce04a786e36c296053b2
gwizdek <gwizdek@f427f6ac-f01a-11de-af0a-9d584d74d99e>
2010-01-14 22:34:11 +0000

    Powrót do plików .h i .cpp (okazuje sie ze tak lepiej nie robic... :] )

--- src/Main/Main.h
+++ src/Main/Main.h
@@ -9,7 +9,6 @@
 #define MAIN_H_
 
 #include <string>
-#include "../debug/debug.h"
 
 using namespace std;
 
@@ -21,28 +20,12 @@ public:
 	/**
 	 * Tak na prawdê jedyne co mo¿emy zrbiæ ze string to wyœwietliæ, zalogowaæ lub nic.
 	 */
-	static void handleError(string message) {
-		PRN("Error: ");
-		PRN(message);
-	}
+	static void handleError(string message);
 
 	/**
 	 * Tu przekazujemy argumenty wywo³ania programu.
 	 */
-	static int startProgram(int argc, char **argv) {
-		PRN1("in startProgram");
-
-		//print arguments
-		for (int i=0; i<argc; i++) cout<<"arg nr "<<i<<" = "<<argv[i]<<endl;
-
-		PRN("TODO: wczytanie konfiguracji");
-		PRN("TODO: wczytuje dane audio");
-		PRN("TODO: wywo³uje odpowiednie metody w VoIPModule..");
-		PRN("TODO: zapisuje loga do pliku...");
-
-		PRN1("ends startProgram");
-		return 0;
-	}
+	static int startProgram(int argc, char **argv);
 
 };
 
--- src/VoIPModule/VoIPModule.h
+++ src/VoIPModule/VoIPModule.h
@@ -18,16 +18,8 @@ public:
 	VoIPModule();
 	virtual ~VoIPModule();
 
-	/**
-	 * Wysy³amy dane z pliku audio do wskazanego hosta zestawiaj¹c najpierw po³¹czenia VoIP.
-	 * TODO: jakie argumenty potrzebne?
-	 */
 	void doSending(string audioFile, int destIP);
 
-	/**
-	 * Czekamy na po³¹czenie, odbieramy je i zapisujemy dane audio do wskazanego pliku.
-	 * TODO: jakie argumenty?
-	 */
 	void doReceiving(string audioFile);
 
 };
