79b8b1dbca3c7bf305516dfb79a15b173b908356
gwizdek <gwizdek@f427f6ac-f01a-11de-af0a-9d584d74d99e>
2010-01-14 22:34:11 +0000

    Powrót do plików .h i .cpp (okazuje sie ze tak lepiej nie robic... :] )

---
+++ trunk/src/Main/Main.cpp
@@ -0,0 +1,39 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 2010-01-14
+ *      Author: Piotr Gwizda³a
+ */
+
+#include "Main.h"
+#include "../debug/debug.h"
+
+Main::Main() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Main::~Main() {
+	// TODO Auto-generated destructor stub
+}
+
+
+void Main::handleError(string message) {
+	PRN("Error: ");
+	PRN(message);
+}
+
+int Main::startProgram(int argc, char **argv) {
+	PRN1("in startProgram");
+
+	//print arguments
+	for (int i=0; i<argc; i++) cout<<"arg nr "<<i<<" = "<<argv[i]<<endl;
+
+	PRN("TODO: wczytanie konfiguracji");
+	PRN("TODO: wczytuje dane audio");
+	PRN("TODO: wywo³uje odpowiednie metody w VoIPModule..");
+	PRN("TODO: zapisuje loga do pliku...");
+
+	PRN1("ends startProgram");
+	return 0;
+}
