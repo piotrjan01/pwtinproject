11818830e07f3030517e673717b0f9ef1dfc3d3e
mlesniew <mlesniew@f427f6ac-f01a-11de-af0a-9d584d74d99e>
2010-01-16 10:58:45 +0000

    Wiecej drobnych poprawek.

--- trunk/src/ProtocolModule/SIP/semaphore.h
+++ trunk/src/ProtocolModule/SIP/semaphore.h
@@ -6,40 +6,40 @@
 #ifndef SEMAPHORE_H
 #define SEMAPHORE_H
 
-/*#include <sys/types.h> 
-#include <sys/stat.h> 
-#include <string.h> 
-#include <errno.h> 
-#include <fcntl.h> 
-#include <pthread.h> 
+/*#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
 #include <unistd.h>*/
 
 #include <semaphore.h>
 
 class Semaphore {
 
-	private:
-		sem_t sem;
+private:
+    sem_t sem;
 
-	public:
-		Semaphore( int value ) {
-			if( sem_init( & sem, 0, value ) != 0 )
-				throw "sem_init: failed";
-		}
+public:
+    Semaphore( int value ) {
+        if ( sem_init( & sem, 0, value ) != 0 )
+            throw "sem_init: failed";
+    }
 
-		~Semaphore() { 
-			sem_destroy( & sem ); 
-		}
+    ~Semaphore() {
+        sem_destroy( & sem );
+    }
 
-		void P() {
-			if( sem_wait( & sem ) != 0 )
-				throw "sem_wait: failed";
-		}
+    void P() {
+        if ( sem_wait( & sem ) != 0 )
+            throw "sem_wait: failed";
+    }
 
-		void V() {
-			if( sem_post( & sem ) != 0 )
-				throw "sem_post: failed";
-		}
+    void V() {
+        if ( sem_post( & sem ) != 0 )
+            throw "sem_post: failed";
+    }
 };
 
 #endif
